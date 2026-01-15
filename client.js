ffd4f2934d5dfb50da6e14c19a0d3b07d72f4bdc
DavidKupka <D23794@oa-opava.cz>
2026-01-15 10:32:41 +0100

    Add files via upload

---
+++ client.js
@@ -0,0 +1,17 @@
+const e = require("express");
+
+const socket = io();
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    socket.emit('chat message', input.value);
+    input.value = '';
+});
+
+socket.on('chat message', (msg) => {
+    const item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+});
